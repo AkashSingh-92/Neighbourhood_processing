90991944a99fd6886ed88752eb60ee17edae9f3d
AkashSingh-92 <85782935+AkashSingh-92@users.noreply.github.com>
2022-02-17 21:00:21 +0530

    Standard High pass filtering
    It is used to view edges of an image

---
+++ Highpass_standard_Filtering.m
@@ -0,0 +1,33 @@
+clc;
+clear all;
+
+% f=[10 10 10 10 10 10 10 10;...
+%    10 10 10 10 10 10 10 10;...
+%    10 10 10 10 10 10 10 10;...
+%    10 10 10 10 10 10 10 10;...
+%    200 200 200 200 200 200 200 200;...
+%    200 200 200 200 200 200 200 200;...
+%    200 200 200 200 200 200 200 200;...
+%    200 200 200 200 200 200 200 200]
+
+f=double(imread('cameraman.tif'));
+
+
+w=[-1 -1 -1;-1 8 -1;-1 -1 -1];    % high pass filtering mask
+
+[row,col]=size(f);
+
+g=f; %making the extreme pixels same as original image
+
+for x=2:1:row-1
+    for y=2:1:col-1
+        g(x,y)=f(x-1,y-1)*w(1,1)+f(x-1,y)*w(1,2)+f(x-1,y+1)*w(1,3)+...
+            f(x,y-1)*w(2,1)+f(x,y)*w(2,2)+f(x,y+1)*w(2,3)+...
+            f(x+1,y-1)*w(3,1)+f(x+1,y)*w(3,2)+f(x+1,y+1)*w(3,3);
+    end
+end
+
+%g
+
+figure(1),imshow(uint8(f)),title('Original image')
+figure(2),imshow(uint8(g)),title('Standard High pass filtered Image')
